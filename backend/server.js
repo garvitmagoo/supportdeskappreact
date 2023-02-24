8e94c7a6a4de43bb17e9b2aefe05a66caa75c908
Garvit Magoo <garvit.magoo@gmail.com>
2023-02-24 11:17:25 +0530

    Backend Code

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-console.log("Server")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const colors = require('colors')
+const PORT = process.env.PORT || 8000
+const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const app = express()
+
+connectDB()
+
+console.log(process.env.PORT)
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Welcome to Support API'})
+})
+
+app.use('/api/users', require('./routes/userRoutes'))
+app.use(errorHandler)
+app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`))
